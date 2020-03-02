12a8e7bac8fca0da6917e92e0b33b279d85f2ad1
Matheus <matheus_barbosa@usp.br>
2020-03-02 11:09:48 -0300

    initial

--- index.js
+++ index.js
@@ -4,18 +4,14 @@ const fs = require ('fs');
 const app = express();
 
 app.use('/static', express.static('static'));
-app.use(bodyParser.urlencoded());
 app.set('view engine', 'ejs');
 
+const data = require('./file.json')
 
-app.get('', (req, res) => {
-    res.render('index');
-});
-
-app.get('/secondpage', (req, res) => {
-    res.render('secondpage'); 
-});
-
+app.get('/', function (req, res) {
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(data));
+})
 
 app.listen(3000, () => {
     console.log('Servidor inicializado na 3000')
